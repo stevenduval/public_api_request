2cbf638bad350693d67a082851de21ed822253d8
Steven Duval <stevenduval@gmail.com>
2020-02-17 00:11:06 -0600

    adding in code for search and prev next btns

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
 //global variables
 const gallery = document.querySelector('#gallery');
+const searchContainer = document.querySelector('.search-container');
+
+// insert loading message if api is slow to respond
+gallery.insertAdjacentHTML('beforebegin', '<div class="loading" style="margin-top: 50vh;">Loading....</div>');
 
 // fetch 12 random users from randomuser API
 fetch('https://randomuser.me/api/?results=12&nat=US')
     // change the response to JSON format
     .then(response => response.json())
     // send the data to the generateEmployee function
-    .then(data => generateEmployee(data));
+    .then(data => generateEmployee(data))
+    // generate search box
+    .then(() => search())
+    //throw an error if something is wrong and place an error message onto the page
+    .catch((error) => gallery.insertAdjacentHTML('beforebegin', `<div class="loading" style="margin-top: 50vh;">Oops something went wrong. Please try again!${error}</div>`))
+    // remove loading message on success
+    .finally(() => document.querySelector('.loading').remove());
 
-function generateEmployee(data) {
+const generateEmployee = (data) => {
     // for each employee do the following
     data.results.forEach(employee => {
         // create a div to store the employee
@@ -34,7 +44,10 @@ function generateEmployee(data) {
         .forEach((card, index) => card.addEventListener('click', () => generateModal(data.results[index-1])));
 }
 
-function generateModal(employee){
+const generateModal = (employee) => {
+    console.log(employee);
+    const dob = new Date(employee.dob.date);
+    const dobNewFormat = (dob.getMonth() + 1) + '/' + dob.getDate() + '/' +  dob.getFullYear();
     // insert the modal into the page when the information is sent over 
     gallery.insertAdjacentHTML('afterend', `
         <div class="modal-container">
@@ -50,8 +63,12 @@ function generateModal(employee){
                 <p class="modal-text">
                     ${employee.location.street.number} ${employee.location.street.name}., ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}
                 </p>
-                <p class="modal-text">Birthday: 10/21/2015</p>
+                <p class="modal-text">Birthday: ${dobNewFormat}</p>
             </div>
+        </div>
+        <div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
         </div>
     `);
     // grab the close icon
@@ -61,3 +78,13 @@ function generateModal(employee){
     // listen for clicks on the close icon and then remove modal if clicked
     close.addEventListener('click', () => modal.remove());
 }
+
+const search = () => {
+    searchContainer.innerHTML = `
+    <form action="#" method="get">
+    <input type="search" id="search-input" class="search-input" placeholder="Search...">
+    <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form>
+    `
+}
+
